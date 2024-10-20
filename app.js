254d200e1cd3950b08959839e4640372d9fc66ac
esr-sosa <sosaemanuel4436@gmail.com>
2024-10-20 00:20:23 -0300

    a

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Middleware
-app.use(express.urlencoded({ extended: false })); // Reemplaza body-parser
-app.use(express.json()); // Reemplaza body-parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
 const indexRouter = require('./routes/index');
 const agendaRouter = require('./routes/agenda');
 const pacienteRouter = require('./routes/paciente');
@@ -20,9 +18,9 @@ app.use('/', indexRouter);
 app.use('/agenda', agendaRouter);
 app.use('/paciente', pacienteRouter);
 
-// Manejo de errores 404
 app.use((req, res, next) => {
   res.status(404).render('404', { title: 'Página no encontrada' });
 });
 
 module.exports = app;
+
