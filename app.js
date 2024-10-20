e70b8832bc8c4362fa7499e7449e185c4fefa69c
esr-sosa <sosaemanuel4436@gmail.com>
2024-10-20 00:50:53 -0300

    a

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+// Solo una vez para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 const indexRouter = require('./routes/index');
 const agendaRouter = require('./routes/agenda');
@@ -23,4 +24,3 @@ app.use((req, res, next) => {
 });
 
 module.exports = app;
-
