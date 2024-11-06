2c638b2daa466a03a59761c8ab1afb43526028c9
maurobenito <141743206+maurobenito@users.noreply.github.com>
2024-11-06 11:01:42 -0300

    Revert "1"
    This reverts commit 55ee9d81c286fa6cd05f27b697631fc3cb5904a8.

--- routes/index copy.js
+++
@@ -1,9 +0,0 @@
-// routes/index.js
-const express = require('express');
-const router = express.Router();
-const mainController = require('../controllers/mainController');
-
-// Ruta principal que renderiza index.pug
-router.get('/', mainController.home);
-
-module.exports = router;
